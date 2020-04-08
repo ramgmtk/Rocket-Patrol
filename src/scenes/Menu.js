3f4eeaf3c480135312ecb87b1d2639c7d37165f7
Ram <ramgmtk@gmail.com>
2020-04-08 15:47:13 -0700

    Scenes & assets implemented

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,4 +2,11 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
+
+    create() {
+        this.add.text(20, 20, "Rocket Patrol Menu");
+
+        //launch the next scene
+        this.scene.start("playScene") //temporary line
+    }
 }
